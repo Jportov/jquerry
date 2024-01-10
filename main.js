904cdcdf1262240703cdd9d2647910c33652c2fc
Jportov <140113109+Jportov@users.noreply.github.com>
2024-01-10 13:22:51 -0300

    implementação de função e alteração no css.
    Foi adicionado uma função que ao clicar duas vezes sobre a tarefa, a linha que sublinha a mesma é removida. Alteração de cores no css.

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
     $('form').on('submit', function(e){
         e.preventDefault();
         const novaTarefa1 = $('#tarefaAdicionar').val();
-        const novoItem = $('<li>✔ </li>')
+        const novoItem = $('<li> </li>')
         $(`<a>${novaTarefa1} </a>`).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $('#tarefaAdicionar').val('')
         $('ul').on('click','li',function(){
-            $(this).css('text-decoration', 'line-through');
-
+            $('li').css('text-decoration', 'line-through');
+            
+        }) 
+        $('ul').on('dblclick','li',function(){
+            $('li').css('text-decoration', 'none');
+            
         }) 
     })
